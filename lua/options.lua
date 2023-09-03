ad91a99d11a4169aa2ee7a16712443ae46cb7bc5
Andres Uris <andres.urismontilla@gmail.com>
2023-09-03 12:16:29 +0000

    improve setup

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,7 @@
 local options = {
   relativenumber = true, -- set relative numbered lines
-  shiftwidth = 4, -- the number of spaces inserted for each indentation
-  tabstop = 4, -- insert 4 spaces for a tab
+  shiftwidth = 4,        -- the number of spaces inserted for each indentation
+  tabstop = 4,           -- insert 4 spaces for a tab
   wrap = false,
 }
 
